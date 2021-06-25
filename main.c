e6d2672a316a058ebf445db47f3f9f0c23e3d8ef
funfish870217 <68422751+funfish870217@users.noreply.github.com>
2021-06-26 01:48:32 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,20 @@
 #include "api.h"
 #include <stdio.h>
 #include <string.h>
-
-
 // The testdata only contains the first 100 mails (mail1 ~ mail100)
 // and 2000 queries for you to debug.
-
 int n_mails;
 int n_queries;
 mail *mails;
 query *queries;
-
 typedef struct MailSite{
 	int key;
     struct MailSite* next; //連接下一個token
 }MailSite;
-
 typedef struct TokenHead{
 	struct Token* head; //連接第一個token
 	struct Token* tail; //連接最後一個token
 }TokenHead;
-
 typedef struct Token{
     char *token; //最常的英文單字應該也只有45個字？
     int len; // token的長度 modify
@@ -47,14 +41,12 @@ Token *addToken(char *word, int key, int len){
     return newtoken;
 }
 
-
 TokenHead *NewHead(char *word, int key, int len){
     TokenHead *newhead = (TokenHead*)malloc(sizeof(TokenHead));
     newhead->head = addToken(word, key, len); //modify
     newhead->tail = newhead->head;
     return newhead;
 }
-
 
 int hash(char const* s, int* len_ptr) {
     const int p = 31;
@@ -73,7 +65,6 @@ int hash(char const* s, int* len_ptr) {
     return hash_value;
 }
 
-
 void tokenize(mail **mails, int n_mails, TokenHead **tokenhead){  //modify
     for(int i = 0; i < n_mails ; i++){
         mail *m = &(*mails)[i];
@@ -81,11 +72,14 @@ void tokenize(mail **mails, int n_mails, TokenHead **tokenhead){  //modify
         char delimit[]=" ,.-':;?()+*/%$#!\"@^&][";
         char *string[1000]; //存放content
         char *string2[256]; //存放subject
+        
         strtok(m->subject, delimit);
         int idx = 0, j = 0, idx2 = 0;
-        
+
         string[idx] = strtok(m->content, delimit);    
+        //string[idx] = strtok(m->content, delimit);
         
+        //content
         while(string[idx] != NULL){
             int h = 0;
             int len = 0; //token長度
@@ -106,12 +100,10 @@ void tokenize(mail **mails, int n_mails, TokenHead **tokenhead){  //modify
                     //printf("雖然collision但是檢查到同一個單詞: ");
                     idx++;
                     string[idx] = strtok(NULL, delimit);
-                    
 					if(i != curr->tail->key){
                         tokenhead[h]->head->tail->next = addSite(i);
                         tokenhead[h]->head->tail = tokenhead[h]->head->tail->next;
                     }
-
                     continue;
                 }
                 
@@ -127,17 +119,79 @@ void tokenize(mail **mails, int n_mails, TokenHead **tokenhead){  //modify
                         curr = curr->next;
                         is_diff = strncmp(curr->token, string[idx], len);
                         
-						if (is_diff == 0){
-                            curr->tail->next = addSite(i);
-                            curr->tail = curr->tail->next;
+                        if (is_diff == 0){
+                            if(i != curr->tail->key){
+                                curr->tail->next = addSite(i);
+                                curr->tail = curr->tail->next;
+                            }
                             break;
-                        }   
+                        }          
                     }
                 }   
             }
             printf("string [%d] = %s\n", idx, string[idx]); 
+            printf("content [%d] = %s\n", idx, string[idx]); 
             idx++;
             string[idx] = strtok(NULL, delimit);
+        }
+
+
+        //subject
+        string2[idx2] = strtok(m->subject, delimit);
+        while(string2[idx2] != NULL){
+            // printf("來到subject");
+            int h = 0;
+            int len = 0; //token長度
+            h = hash(string2[idx2], &len);
+            printf("%d ", h);
+            if(tokenhead[h] == NULL)  //目前hash_table這格是空的
+                tokenhead[h] = NewHead(string2[idx2], i, len);
+
+            else{
+                //printf("collision\n");
+                Token *curr = tokenhead[h]->head;
+                //printf("目前在位的單詞 %s\n", curr->token);
+
+                int is_diff = 0;
+                is_diff = strncmp(curr->token, string2[idx2], len);
+
+                if(is_diff == 0){
+                    //printf("雖然collision但是檢查到同一個單詞: ");
+                    idx2++;
+                    string2[idx2] = strtok(NULL, delimit);
+
+					if(i != curr->tail->key){
+                        tokenhead[h]->head->tail->next = addSite(i);
+                        tokenhead[h]->head->tail = tokenhead[h]->head->tail->next;
+                    }
+                    continue;
+                }
+
+                while(is_diff != 0){
+                    printf("發生collision且是不同的單詞：\n: ");
+                    //如果直到最後都沒有檢查到符合者
+                    if(curr->next == NULL){
+                        tokenhead[h]->tail->next = addToken(string2[idx2], i, len);
+                        tokenhead[h]->tail = tokenhead[h]->tail->next; //update tail
+                        break;
+                    }
+                    else{
+                        curr = curr->next;
+                        is_diff = strncmp(curr->token, string2[idx2], len);
+						
+                        if (is_diff == 0){
+                            if(i != curr->tail->key){
+                                curr->tail->next = addSite(i);
+                                curr->tail = curr->tail->next;
+                            }
+                            break;
+                        }                           
+                    }
+                }   
+            }
+            printf("subject [%d] = %s\n", idx2, string2[idx2]); 
+            idx2++;
+            string2[idx2] = strtok(NULL, delimit);
         }
     } 
 }
@@ -176,6 +230,5 @@ int main(void){
 	// 		//printf("%s\n", expression_list[7970]);
 	//    		//api.answer(queries[i].id, NULL, 0);
 	// 	}
-
   return 0;
 }
